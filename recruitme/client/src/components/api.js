678d15d5fc52b7192c44dac3b848af10bfd5db96
Jawad Jawid <jawadadham98@gmail.com>
2022-03-14 19:45:17 -0400

    some changes

---
+++ recruitme/client/src/components/api.js
@@ -0,0 +1,17 @@
+function send(method, url, data, callback){
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (xhr.status !== 200) callback("[" + xhr.status + "]" + xhr.responseText, null);
+        else callback(null, JSON.parse(xhr.responseText));
+    };
+    xhr.open(method, url, true);
+    if (!data) xhr.send();
+    else{
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+    }
+}  
+
+export function getUsername(){
+    return document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+}
