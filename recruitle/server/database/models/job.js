0c7bdfe83b0bce9c197c64bbb4fc0d7300248dc7
Ali Al Rawaf <alrawafali@gmail.com>
2022-03-20 13:43:33 -0400

    add job schema

---
+++ recruitle/server/database/models/job.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
+
+const JobSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    companyName:{
+        type: String,
+        required: true
+    },
+    salary:{
+        type: Number,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+
+});
+
+JobSchema.set('timestamps', true);
+JobSchema.plugin(uniqueValidator);
+
+module.exports = Job = mongoose.model('Job', JobSchema);
