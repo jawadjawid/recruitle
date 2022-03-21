401341b175c165cf717d0b1cbeee820fe4d19c99
Osman Amjad <m.osman.amjad@gmail.com>
2022-03-20 21:10:34 -0400

    remove unused

--- recruitle/server/profile.js
+++ recruitle/server/profile.js
@@ -1,11 +1,10 @@
-const applicant = require('./database/models/applicant');
-const employer = require('./database/models/employer');
+const Applicant = require('./database/models/applicant');
 
 module.exports = {
     uploadResume: (req, res, next) => {
         let id = req.username;
         let file = req.file;
-        applicant.updateOne({_id: id}, {$set: {resume: file}}, function(err, raw) {
+        Applicant.updateOne({_id: id}, {$set: {resume: file}}, function(err, raw) {
             //if (err) return res.status(500).end(err);
             console.log(raw)
             return res.json(raw)
@@ -13,7 +12,7 @@ module.exports = {
     },
 
     getResume: (req, res, next) => {
-        applicant.findOne({_id: req.params.id}, function(err, app){
+        Applicant.findOne({_id: req.params.id}, function(err, app){
             if (err) return res.status(500).end(err);
             if (!app) return res.status(404).end("User with id: " + req.params.id + " does not exist");
             res.setHeader('Content-Type', app.resume.get('mimetype'));
