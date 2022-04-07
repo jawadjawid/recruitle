81523cb87a077c0c31d016e8c7f2cbf01624ae2a
Jawad Jawid <jawadadham98@gmail.com>
2022-04-07 12:47:36 -0400

    Update NavBar.js

--- recruitle/client/src/components/NavBar.js
+++ recruitle/client/src/components/NavBar.js
@@ -45,7 +45,7 @@ export default function NavBar(props) {
             {isJobsPage && <Form onSubmit={search} className="d-flex">
           <FormControl
             type="search"
-            placeholder="Search ozman"
+            placeholder="Search ali"
             className="me-2"
             aria-label="Search"
           />
